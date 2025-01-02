f7b5f93edd67b1c75e33c3a55dd40ed2aef03dd4
Tobias Baer <Tobias.Baer@web.de>
2025-01-02 02:35:56 +0100

    fixup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vokatus/irregular_words/irregular_word_page.dart';
+import 'package:vokatus/irregular_verbs/irregular_word_page.dart';
 
 void main() {
   runApp(const VokatusApp());
