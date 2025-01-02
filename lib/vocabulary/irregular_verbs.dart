cb53d1be73b1167a6dadc6c9050f61cbb75ee8f3
Tobias Baer <Tobias.Baer@web.de>
2025-01-02 02:35:30 +0100

    Add IrregularVerb class and initial list of irregular verbs

---
+++ lib/vocabulary/irregular_verbs.dart
@@ -0,0 +1,35 @@
+class IrregularVerb {
+  final String german;
+  final String infinitive;
+  final String simplePast;
+  final String pastParticiple;
+
+  IrregularVerb(
+      {required this.german,
+      required this.infinitive,
+      required this.simplePast,
+      required this.pastParticiple});
+}
+
+List<IrregularVerb> irregularVerbs = [
+  IrregularVerb(
+      german: "kaufen",
+      infinitive: "to buy",
+      simplePast: "bought",
+      pastParticiple: "bought"),
+  IrregularVerb(
+      german: "bringen",
+      infinitive: "to bring",
+      simplePast: "brought",
+      pastParticiple: "brought"),
+  IrregularVerb(
+      german: "bauen",
+      infinitive: "to build",
+      simplePast: "built",
+      pastParticiple: "built"),
+  IrregularVerb(
+      german: "lehren",
+      infinitive: "to teach",
+      simplePast: "taught",
+      pastParticiple: "taught"),
+];
